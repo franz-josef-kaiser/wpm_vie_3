28eccc1067026074e7d42f9426a95cd9fd905f15
Kaiser Franz Josef <wecodemore@gmail.com>
2013-04-03 02:22:49 +0200

    close to a working slidedeck

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -1,5 +1,36 @@
 ;( function($) {
 	"use strict";
 
-	var slides = $( '.slide' );
+	$( document.documentElement ).keyup( function( e ) {
+		event.preventDefault();
+
+		var key_code = e.keyCode || e.which,
+			arrow = { left: 37, up: 38, right: 39, down: 40 },
+			slide_active = $( '.slide.active' ),
+			slide_target;
+
+		switch ( key_code ) {
+			// Left/Previous
+			case arrow.left:
+			case arrow.up:
+				if ( slide_active.prev( '.slide' ).length ) {
+					slide_target = slide_active.prev( '.slide' );
+				} else {
+					slide_target = $( '.slide:last' );
+				}
+				break;
+
+			// Right/Next
+			case arrow.right:
+			case arrow.down:
+				if ( slide_active.next( '.slide' ).length ) {
+					slide_target = slide_active.next( '.slide' );
+				} else {
+					slide_target = $( '.slide:first' );
+				}
+				break;
+		}
+		slide_target.addClass( 'active' );
+		slide_active.removeClass( 'active' );
+	} );
 } )( jQuery );
