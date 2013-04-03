c95576a2ff4c29ef7975d936e35dd0fa07290b43
Kaiser Franz Josef <wecodemore@gmail.com>
2013-04-04 01:17:02 +0200

    base structure

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -6,31 +6,37 @@
 
 		var key_code = e.keyCode || e.which,
 			arrow = { left: 37, up: 38, right: 39, down: 40 },
-			slide_active = $( '.slide.active' ),
-			slide_target;
+			$slide_active = $( '.slide.active' ),
+			$slide_target;
 
 		switch ( key_code ) {
 			// Left/Previous
-			case arrow.left:
-			case arrow.up:
-				if ( slide_active.prev( '.slide' ).length ) {
-					slide_target = slide_active.prev( '.slide' );
+			case arrow.left :
+			case arrow.up :
+				if ( $slide_active.prev( '.slide' ).length ) {
+					$slide_target = $slide_active.prev( '.slide' );
 				} else {
-					slide_target = $( '.slide:last' );
+					$slide_target = $( '.slide:last' );
 				}
 				break;
 
 			// Right/Next
-			case arrow.right:
-			case arrow.down:
-				if ( slide_active.next( '.slide' ).length ) {
-					slide_target = slide_active.next( '.slide' );
+			case arrow.right :
+			case arrow.down :
+				if ( $slide_active.next( '.slide' ).length ) {
+					$slide_target = $slide_active.next( '.slide' );
 				} else {
-					slide_target = $( '.slide:first' );
+					$slide_target = $( '.slide:first' );
 				}
 				break;
 		}
-		slide_target.addClass( 'active' );
-		slide_active.removeClass( 'active' );
+		$slide_target.addClass( 'active' );
+		$slide_active.removeClass( 'active' );
+		// $slide_target.slideToggle( 'slow' );
+		// $slide_active.sideToggle( 'slow' );
+		// $slide_target.fadeToggle( 'slow' );
+		// $slide_active.toggle( 'slow' );
+		// $slide_active.hide( 'slide', { direction: 'left' }, 1000 );
+		// $slide_target.show( 'slide', { direction: 'right' }, 1000 );
 	} );
 } )( jQuery );
